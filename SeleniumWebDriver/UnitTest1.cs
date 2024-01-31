05eefbbcb0513f281cb3ace95e37cf854706f1c6
Maksim9056 <bobreczovm@inbox.ru>
2024-01-31 16:21:43 +0300

    Драйвер Chrome

--- SeleniumWebDriver/UnitTest1.cs
+++ SeleniumWebDriver/UnitTest1.cs
@@ -2,7 +2,7 @@ using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Edge;
 using OpenQA.Selenium.Support.UI;
-using System;
+using OpenQA.Selenium.Chrome;
 
 namespace SeleniumWebDriver
 {
@@ -11,9 +11,14 @@ namespace SeleniumWebDriver
         [Test]
         public void FirstSeleniumTest()
         {
-            var edgeOptions = new EdgeOptions();
+            //var edgeOptions = new EdgeOptions();
+            //var edgeOptions = new ChromeOptions();
 
-            IWebDriver driver = new EdgeDriver(@"C:\edgedriver_win64\msedgedriver.exe", edgeOptions);        
+            //IWebDriver driver = new EdgeDriver(@"C:\edgedriver_win64\msedgedriver.exe", edgeOptions);
+            var chromeOptions = new ChromeOptions();
+            IWebDriver driver = new ChromeDriver(@"C:\chromedriver-win64\", chromeOptions);
+
+
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
             driver.Navigate().GoToUrl("https://konflic.github.io/examples/editor/index.html");
 
